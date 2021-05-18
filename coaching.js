a8731e132fad5498ecab11dcf17581b3c7ad2fec
Gregor <gregorlthomson@sky.com>
2021-05-18 18:26:19 +0100

    Work on step two

--- coaching.js
+++ coaching.js
@@ -3,8 +3,7 @@
 // Get all numbers from the document and add the ELEMENTS to an array
 // We'll handle the values later on
 
-const numbers = document.getElementsByClassName("number");
-const numArr = Array.from(numbers);
+const numArr = Array.from(document.getElementsByClassName("number"));
 const clickArr = [];
 
 // When a number is clicked, add the element to an array of 'active numbers'
@@ -38,6 +37,42 @@ const newNum = numArr.map(function(number){
         addActiveClass(event.target);
     });
 });
+
+// PART TWO
+
+// Get the button, and the radio button inputs
+
+const goButton = document.getElementById("go-button");
+const h3 = document.getElementById("result-output");
+const doubler = document.getElementById("doubler");
+const multiples = document.getElementById("multiples");
+const radioArray = [doubler, multiples];
+
+// Write a function that will output the numbers selected
+// Replace the text 'Result' with the value of the first element of our array of clicked elements
+
+function getsValuesFromElements(element){
+    // Return the textContent of that element
+    return element.textContent;
+}
+
+function replaceResult(){
+    // use getsValuesFromElements() to map the textContent of each element into a new array. The new array will hold all of the 'values' i.e. the textContent
+    const textArray = clickArr.map(element=>{
+        return parseInt(getsValuesFromElements(element));
+    })
+
+    console.log(textArray[0] + textArray[1])
+
+    h3.textContent = clickArr[0].textContent;
+}
+
+// Add an eventListener so that when the button is clicked, replace 'Result' text with the result
+
+goButton.addEventListener("click", replaceResult);
+
+
+
 
 
 
