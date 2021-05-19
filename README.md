df4ef41354bb69ef382d5ef8c5f0d53a5b26d59b
Gregor <gregorlthomson@sky.com>
2021-05-19 11:39:39 +0100

    Add steps to the README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # array-methods
+
+Given a collection of numbers, we can select up to five and perform an algorithm on those numbers. Our result will appear as an `h3` below the radio buttons.
+
+---
+
+## Getting started
+
+To continue working on this repository from where we left off in coaching (Tuesday 18 May):
+
+1. Fork this repository
+1. Clone it to your local machine using `git clone`
+1. Open your code editor
+1. Follow steps three and four in `coaching.js`
+
+To start working on this repository from scratch:
+
+1. Fork this repository
+1. Clone it to your local machine using `git clone`
+1. Open your code editor
+1. Follow the steps in `attempt.js`
---
+++ attempt.js
@@ -0,0 +1,48 @@
+// PART ONE
+
+// Get all numbers from the document and add the ELEMENTS to an array
+// We'll handle the values later on
+
+// When a number is clicked, add the element to an array of 'active numbers'
+
+// Check whether the array already contains the element
+// If it does, don't add it.
+
+// When a number is clicked, give it the active class
+// And remove the class if the element has the class already
+
+// If a sixth element is clicked, remove the first element in the array
+// Add the sixth element
+
+
+// PART TWO
+
+// Get the button, and the radio button inputs
+
+
+// Write a function that will output the numbers selected
+// Replace the text 'Result' with the value of the first element of our array of clicked elements
+
+// Write a helper function to convert the text in each element to its numeric value
+
+
+
+// Add an eventListener so that when the button is clicked, replace 'Result' text with the result
+
+
+// PART THREE
+
+// Now let's write specific functions for doubling and dividing (multiples)
+// We'll call these functions inside replaceResult
+
+
+// PART FOUR
+
+// Include a conditional statement in the replaceResult function which runs either function based on which radio button is clicked. 
+
+
+
+
+
+
+
